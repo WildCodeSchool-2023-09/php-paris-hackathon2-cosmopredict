62148329ec333ccc4b2694af9c4f50d664dd8866
nicolasalibert <contact@nicolasalibert.me>
2024-01-18 11:26:19 +0100

    [ADDED] Consumer, Order and Product entities

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Service\PredictionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/predictions', name: 'predictions')]
+    public function showPredictions(PredictionService $predictionService): Response
+    {
+        $predictions = $predictionService->predict([]);
+
+        return $this->render('home/index.html.twig', [
+            'prediction' => $predictions
+        ]);
     }
 }
