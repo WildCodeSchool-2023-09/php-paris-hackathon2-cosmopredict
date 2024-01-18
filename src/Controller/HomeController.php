38f4869e3e837068b9c7548e8c47f34e7f64caeb
Baptiste Delefosse <baptiste.delefosse@hotmail.fr>
2024-01-18 16:12:55 +0100

    Do html and css and vtf

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,15 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            return $this->render('home/result.html.twig', [
+                'contentTitle' => 'Futures tendances - Prévisions',
+                'form_data' => $_POST
+            ]);
+        }
+
+        return $this->render('home/index.html.twig', [
+            'contentTitle' => 'Rechercher les futures tendances'
+        ]);
     }
 }
